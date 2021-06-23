0aaaf5cc9b5659c1985d30a23af7faeb69bed011
DTM-BD <dungtmdspk@gmail.com>
2021-06-23 12:54:43 +0700

    so sanh tung pixel cua 2 anh, xuat anh theo tung mau (RGB), tru 2 anh cho nhau

--- python2images.py
+++ python2images.py
@@ -1,14 +1,18 @@
 import cv2
 #import nunpy as np
 
-original = cv2.imread("181002113456-01-golden-gate-bridge-restricted.jpg")
-duplicate = cv2.imread("181002113456-01-golden-gate-bridge-restricted.jpg")
+original = cv2.imread("3545122068_5bf27ae7c4_b.jpg")
+duplicate = cv2.imread("3545122068_5bf27ae7c4_c.jpg")
 
 # 1) check if 2 images are equals
 if original.shape == duplicate.shape:
 	print("Image same size and channel")
-	difference = cv2.subtract(original, duplicate)
+	difference = cv2.subtract(original, duplicate) # tru pixel 2 hinh, va ve su khac nhau
+#	cv2.imshow("difference", difference)
 	b, g, r = cv2.split(difference)
+#	cv2.imshow("b", b) #show hinh chi voi mau blue
+#	cv2.imshow("g", g) #show hinh chi voi mau green
+#	cv2.imshow("r", r) #show hinh chi voi mau red
 
 	if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r)  == 0:
 		print("The image are completetly equal")
@@ -17,7 +21,7 @@ if original.shape == duplicate.shape:
 
 
 
-cv2.imshow("Orginal", original)
+#cv2.imshow("Orginal", original)
 cv2.imshow("duplicate", duplicate)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
